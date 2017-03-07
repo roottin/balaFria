1fa729a8be927db1f2837706ad3272da2d37f472
vleon <vleon@socaportuguesa.com>
2017-03-07 11:58:59 -0400

    cambio a boneCrusher

---
+++ Client/helpers/catalogo.js
@@ -0,0 +1,75 @@
+var map;
+function construirUI(){
+	bone.usarLib('customMarker')
+		.then(function(lib){
+			//armo el contenedor
+			var contMapa = UI.agregarVentana({
+				nombre:'contMapa',
+				clases:['contMapa'],
+				sectores:[
+					{
+						nombre:'mapa',
+						html:'<div mapa></div>'
+					}
+				]
+			},document.querySelector('div[contenedor]'));
+			//agrego el mapa
+			map = new google.maps.Map(contMapa.buscarSector('mapa').nodo, {
+				center: {lat: 9.55972, lng: -69.20194},
+				zoom: 13,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			});
+			return map;
+		})
+		.then(function(){
+			var lista = UI.agregarLista({
+				titulo: 'Tiendas',
+				nombre: 'tiendas',
+				clases: ['completa','tiendas'],
+				campo_nombre: 'nombre',
+				carga: {
+					uso:true,
+					peticion:{
+						modulo:'global',
+						entidad:'proveedor',
+						operacion:'listar'
+					},
+					espera:{
+						cuadro:{
+							nombre: 'cargando',
+							mensaje: 'cargando locales'
+						}
+					},
+					respuesta:function(lista){
+						lista.Slots.forEach(function(slot){
+							armarTienda(slot);
+							agregarTiendasAlMapa(map,slot);
+						});
+					}
+				},
+				onclickSlot: abrirTienda
+			},document.querySelector('div[contenedor]'));
+		});
+}
+//---------------------------------------------- Manejo de tiendas --------------------------------------
+var armarTienda = function(slot){
+	var datos = slot.atributos;
+	slot.nodo.classList.add('tienda');
+	var html = "<div parte-izq><img class='muestra' src='img/"+slot.atributos.img+"'></img></div>"+
+				"<div parte-der>"+
+					"<div titulo>"+datos.nombre+"</div>"+
+					"<div detalle>"+datos.descripcion+"</div>"+
+				"</div>";
+	slot.nodo.innerHTML = html;
+};
+var agregarTiendasAlMapa = function(mapa,tienda){
+		tienda.marker = new CustomMarker({
+		    position: new google.maps.LatLng(parseFloat(tienda.atributos.lat),parseFloat(tienda.atributos.lng)),
+		    map: mapa,
+		    img:'img/thumbnails/'+tienda.atributos.img,
+				id: tienda.atributos.id,
+				click: function(){
+					abrirTienda(tienda);
+				}
+	  	});
+};
