7bbdde4aa5be94a9ff527dacd080544612d74333
vleon <vleon@socaportuguesa.com>
2017-03-15 11:40:57 -0400

    cambios

--- Servidor/Core/Core.js
+++ Servidor/Core/Core.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
-pg.defaults.ssl = true;
-connectionString = process.env.DATABASE_URL ;
+//pg.defaults.ssl = true;
+//connectionString = process.env.DATABASE_URL ;
 //connectionString = 'postgres://postgres:1234@localhost:5432/balafria';
 var client = new pg.Client(connectionString);
 client.connect();
