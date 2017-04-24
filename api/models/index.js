364eede9cef1348a61140768db7c436a554e71d0
javier-aranguren <aranguren_3@hotmail.com>
2017-04-24 09:36:52 -0400

    cambio conexion

--- api/models/index.js
+++ api/models/index.js
@@ -7,11 +7,40 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../baseDatos/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env.DATABASE_URL) {
+  var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+
+  console.log('----------------------------------------');
+  console.log(process.env.DATABASE_URL);
+  console.log('----------------------------------------');
+  var sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging: false,
+    dialectOptions: {
+        ssl: true
+    }
+  });
 } else {
+  var url = 'postgres://aabewftefmafkl:f405f64143e5997d8ed016e55bf6b95be5aece2247e2c2ca37c20fa91c63b89e@ec2-54-221-201-244.compute-1.amazonaws.com:5432/d9930ros0uu0lf';
+  console.log('----------------------------------------');
+  console.log(url);
+  console.log('----------------------------------------');
+  /*
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  */
+  var sequelize = new Sequelize(url, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+  });
+
 }
+
 
 fs
   .readdirSync(__dirname)
