32a8d9ebd8847c58b4e57548668cf174190060da
vleon-socaportuguesa <vleon@SOCA-PORTUGUESA.LOCAL>
2017-06-01 14:03:17 -0400

    cambios sucursal

--- cliente/helpers/proveedor/controladores/proveedor-front.js
+++ cliente/helpers/proveedor/controladores/proveedor-front.js
@@ -140,19 +140,23 @@ angular.module('balafria')
 }])
 .controller('ctrlSucursal', ['$state','Sucursales','$scope','$timeout', function ($state,Sucursales,$scope,$timeout){
   var yo = this;
-  yo.temp = {
-    "banner":{
-      "ruta":null,
-      "cambio":false
-    }
-  };
+  yo.edit=false;
+  yo.datos=null;
+  yo.temp=null;
+  yo.icono = "edit";
+
   if(!$state.params.sucursal){
     $state.go('proveedor.dashboard');
   }
-  yo.datos = Sucursales.get({id:$state.params.sucursal},function(){
-    if(yo.datos.banner){
-      yo.temp.banner = yo.datos.banner;
-    }
+  Sucursales.get({id:$state.params.sucursal},function(result){
+    $timeout(function(){
+      yo.datos = angular.copy(result);
+      yo.temp = angular.copy(result);
+      if(!result.banner){
+        yo.datos.banner = {};
+        yo.temp.banner = {};
+      }
+    });
   });
   yo.cambioBanner = function(){
     document.querySelector('input[type="file"]').click();
@@ -163,8 +167,36 @@ angular.module('balafria')
     }
     document.querySelector('#banner').setAttribute('src',yo.temp.banner.ruta);
     $timeout(function(){
-      yo.temp.banner.cambio = true;
-      console.log(yo.temp.banner);
+      yo.temp.cambio = true;
+    });
+  }
+  yo.guardarCambios = function(){
+    console.log(yo.temp);
+  }
+  yo.revertirCambios = function(){
+    if(yo.temp.banner){
+        window.URL.revokeObjectURL(document.querySelector('#banner').getAttribute('src'));
+        document.querySelector('#banner').setAttribute('src',(yo.datos.banner.ruta || ""));
+    }
+    $timeout(function(){
+      yo.temp = angular.copy(yo.datos);
+      yo.temp.cambio = false;
+    });
+  }
+  yo.editar = function(){
+    var cambio = yo.temp.cambio;
+    yo.temp.nombre = document.querySelector('#nombre').value;
+    if(yo.datos.nombre != yo.temp.nombre){
+      cambio = true;
+    }
+    $timeout(function(){
+      yo.edit = !yo.edit;
+      if(!yo.edit){
+        yo.icono="edit";
+      }else{
+        yo.icono="close";
+      }
+      yo.temp.cambio = cambio;
     });
   }
 }]);
