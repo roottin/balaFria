54e333a220b9fe7e0ca7cf6df4735f18d9a7af36
Victor VL. Leon <vleon@SOCA-PORTUGUESA.LOCAL>
2017-09-14 15:31:07 -0400

    cerra 2

--- cliente/helpers/cliente/controladores/cart.js
+++ cliente/helpers/cliente/controladores/cart.js
@@ -4,9 +4,17 @@ angular.module('balafria')
   yo.nodo = document.querySelector('div.omni-cart');
   //------------------ Eventos ---------------------------------
   yo.nodo.ondblclick=function(nodo){
-    yo.nodo.classList.remove('cerrado');
-    yo.nodo.classList.add('abierto');
-    yo.nodo.classList.add('abrir');
+    if(yo.nodo.classList.contains('abrir')){
+      yo.nodo.classList.add('cerrado');
+      yo.nodo.classList.add('cerrar');
+      yo.nodo.classList.remove('abierto');
+      yo.nodo.classList.remove('abrir');
+    }else{
+      yo.nodo.classList.remove('cerrado');
+      yo.nodo.classList.remove('cerrar');
+      yo.nodo.classList.add('abierto');
+      yo.nodo.classList.add('abrir');
+    }
   };
   $scope.$on('actCart',function(){
     yo.nodo.classList.remove('inactivo');
